b6f1cae22d422af63db4cf5d1551d77338f983cf
Juan <juanxsalazar21@gmail.com>
2019-02-07 15:59:52 -0500

    made card shape css

--- public/main.js
+++ public/main.js
@@ -1,11 +1,12 @@
 let number = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
-let suit = ["Hearts", "Diamonds", "Spades", "Clubs"];
+let suit = ["♥", "♦", "♠", "♣"];
 let deck = []
 
-function randomCard() {
+const randomCard = () => {
     let numbers = number[Math.floor(Math.random() * number.length)];
     let suits = suit[Math.floor(Math.random() * suit.length)];
-    document.querySelector("#face").textContent = numbers + ' of ' + suits;
-}
+    document.querySelector("#face").textContent = numbers + suits;
+} 
 
 document.addEventListener('DOMContentLoaded', main)
+
